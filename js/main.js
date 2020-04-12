92527af027414a53e97ef080bdfa7e9557dc9bbb
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-04-13 00:39:48 +1000

    Many changes to styles, variables, css, etc

--- js/main.js
+++ js/main.js
@@ -42,3 +42,68 @@ $(document).ready(function () {
         }
     })
 });
+
+// Creating the active class on the buttons for the Latest work section
+
+const filterButtons = document.querySelector("#filter-btns").children;
+const items = document.querySelector(".portfolio-gallery").children;
+
+for(let i = 0; i<filterButtons.length; i++){
+    filterButtons[i].addEventListener("click",function(){
+        for (let j = 0; j < filterButtons.length; j++){
+            filterButtons[j].classList.remove("active")
+        }
+        this.classList.add("active");
+        const target = this.getAttribute("data-target")
+
+        for(let k = 0; k < items.length; k++){
+            items[k].style.display = "none";
+            if(target===items[k].getAttribute("data-id"))
+            {
+                items[k].style.display = "block";
+            }
+            if(target==="all") {
+                items[k].style.display = "block";
+            }
+        }
+    })
+} 
+
+// Closing lightbox from clicking outside of the image
+lightbox.addEventListener("click",function(){
+    event.preventDefault();
+    lightboxImage = "";
+    if(event.target != lightboxImage) {
+        lightbox.classList.remove("show");
+        lightbox.classList.add("hide");
+    }
+})
+
+// Creating funtion for the close of the lightbox clicking on the X
+closeLightbox.addEventListener("click",function(){
+        event.preventDefault();
+        event.stopPropagation();
+        
+        lightbox.classList.remove("show");
+        lightbox.classList.add("hide");
+})
+
+// When clicking on the plus icon, show the lightbox
+galleryItem.forEach(function(element){
+    
+    element.querySelector(".fas-plus").addEventListener("click", function(){
+        event.preventDefault();
+        event.stopPropagation();
+        lightbox.classList.remove("hide");
+        lightbox.classList.add("show");
+        console.log(light)
+        // changing the image source on every different img
+        //let portfolios = ;
+        portfolios = element.querySelector("img").getAttribute("src");
+       
+        let newImg = document.createElement("img")
+        newImg.setAttribute("src",portfolios)
+        lightbox.append(newImg)
+        return(lightbox)
+    })
+})
